4ba5443a66d7dd68fd5a4cf2d81fe9a0ed63c65a
kessy-m <kessymuriithi@gmail.com>
2024-04-12 16:48:21 +0300

    conditiontask

---
+++ binding.js
@@ -0,0 +1,11 @@
+// getting js data to html
+
+let f_name="kessy"
+let s_name="haddasar"
+let full_name=f_name+s_name
+document.getElementById("first").innerHTML=full_name
+
+
+// getting html to js
+let form = document.getElementById('myform')
+form.addEventListener()
